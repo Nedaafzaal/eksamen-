1f1309126fa317519492a380fa4f4af7ef033372
amalieanderseen <aman24ab@student.cbs.dk>
2025-03-20 11:16:03 +0100

    ny version

--- app.js
+++ app.js
@@ -6,15 +6,32 @@ const express = require("express"); //vi kalder på express pakken
 const app = express(); //appen er lig vores pakke 
 const port = 3001; //definere hvilken port vores server skal køre på 
 
-app.get("view engine", "ejs"); //fortæller serveren at den skal bruge EJS som view engine
+app.set("view engine", "ejs"); //fortæller serveren at den skal bruge EJS som view engine
+app.use(express.urlencoded({extended: true })); //gør at app kan læse data fra en HTML formular 
 
-app.get("/test", (req, res) => {
-    res.status(200).json({message:"Test API has been hit"}) //slet efter kun en test 
+let database =[]; //vores database
+
+app.get("/", (req, res) => {
+    res.render("frontend.ejs"); //når der sendes en get request til serveren så svare serveren med en HTML side fra frontend.ejs
+})
+
+app.get("/opretbruger", (req, res) => {
+    res.render("opretbruger.ejs"); //når der sendes en get request til serveren så svare serveren med en HTML side fra opretbruger.ejs
+})
+
+app.get("/brugere", (req, res) => {
+    res.status(200).json({ brugere: database}); //henter data fra databasen
+})
+
+app.post("/brugere", (req, res) => { //opretter en sti til klienten så de kan sende en request om at logge ind 
+    console.log(req.body); //logger den data klienten har sendt
+    database.push(req.body.name); //pusher nye brugere ind i databasen 
+   res.status(200).json({ brugere: database}); //serveren er fejlfri
 });
 
-app.post("/register", (req, res) => { //opretter en sti til klienten så de kan sende en request 
+   app.post("/opretbruger", (req, res) => { //opretter en sti til klienten så de kan sende en request om at oprette bruger
     console.log(req.body); //logger den data klienten har sendt
-   res.status(200);}) //sender respons tilbage 
+   res.status(200);}) //serveren er fejlfri
 
 app.listen(port, () => {  //hvis port ændres i konstanten ændres det også her derfor kaldes den port
     console.log(`Example app listening on port http://localhost:${port}`) //printer linket i konsollen så vi kan komme ind på webapplikationen
