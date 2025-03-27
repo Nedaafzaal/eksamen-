d91346bc9f92cedb3497a08124b87f2126e4d136
Yas584 <yakh24ac@student.cbs.dk>
2025-03-27 10:14:02 +0100

    ny version

--- app.js
+++ app.js
@@ -10,37 +10,44 @@ app.use(express.static('public')) //Vi sætter alle vores statiske filer indenun
 
 let database =[]; //vores database
 
-app.get("/", (req, res) => {
-    res.render("frontend.ejs"); //når der sendes en get request til serveren så svare serveren med en HTML side fra frontend.ejs
+app.get("/", (req, res) => {                              
+    res.render('dashboard.ejs')
+   }) 
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs"); //når der sendes en get request til serveren så svare serveren med en HTML side fra frontend.ejs
+})
+app.post("/login", (req, res) => {
+    // login logik
+    res.redirect("http://localhost:3001/"); //når der sendes en get request til serveren så svare serveren med en HTML side fra frontend.ejs
 })
 
 app.get("/opretbruger", (req, res) => {
     res.render("opretbruger.ejs"); //når der sendes en get request til serveren så svare serveren med en HTML side fra opretbruger.ejs
 })
 
-app.get("/brugere", (req, res) => {
-    res.status(200).json({ brugere: database}); //henter data fra databasen
-})
 
-app.post("/brugere", (req, res) => { //opretter en sti til klienten så de kan sende en request om at logge ind 
+app.post("/opretbruger", (req, res) => { //opretter en sti til klienten så de kan sende en request om at logge ind 
     console.log(req.body); //logger den data klienten har sendt
     database.push(req.body.name); //pusher nye brugere ind i databasen 
-   res.status(200).json({ brugere: database}); //serveren er fejlfri
+   res.status(200).redirect("http://localhost:3001/"); //serveren er fejlfri
 });
 
-   app.post("/opretbruger", (req, res) => { //opretter en sti til klienten så de kan sende en request om at oprette bruger
-    console.log(req.body); //logger den data klienten har sendt
-   res.status(200);}) //serveren er fejlfri
+
+
+
 
 app.listen(port, () => {  //hvis port ændres i konstanten ændres det også her derfor kaldes den port
     console.log(`Example app listening on port http://localhost:${port}`) //printer linket i konsollen så vi kan komme ind på webapplikationen
-
-/*app.post("/brugere", (req, res) => { //opretter en sti til klienten så de kan sende en request om at oprette bruger
-        console.log(req.body); //logger den data klienten har sendt
-       res.status(200);}) //serveren er fejlfri*/
-});
+})
 
 
+// Session cookies
+// Express redirect
+// ekstra: express middleware
+// Router
+// HTTP statuskoder
+// HTTP protokol meget kort 
 
 
 
