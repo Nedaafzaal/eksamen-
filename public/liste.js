6808ac702aaea8f3d346a19239e1aade26e215e2
amalieanderseen <aman24ab@student.cbs.dk>
2025-04-29 21:25:52 +0200

    kommentar
    hej

--- public/liste.js
+++ public/liste.js
@@ -1,27 +1,27 @@
+
 //Vi laver en funktion som viser listen af oversigt. Dette gør vi for at undgå duplikering og mere orden i koden, da vi flere steder skal vise oversigten
 function lavListe(data, egenskab, fejlBesked) {
-  if (data.length === 0) { // Hvis data ikke eksisterer, returnerer den fejlbesked
+  if (data.length === 0) { //Hvis data ikke eksisterer, returnerer den fejlbesked
       return `<p>${fejlBesked}</p>`;
   }
 
-  let html = ''; // Starter med en tom tekststreng.
+  let html = ''; //Starter med en tom string
 
-  data.forEach(element => { // For hvert element i tabellen. Fx én konto, én transaktion -> Altså den tager et element, fx "Lønkont"
+  data.forEach(element => { 
 
-      html += `<div class="række">`; // Skal den lave en div med klassen "række"
+      html += `<div class="række">`; //Tilføjer en div med klassen den "række"
       
-      egenskab.forEach(egenskab => { // Den gennemgår hvert egenskab (kontonavn, saldo, valuta fx.) Tjekker igennem om lønkonto har saldo, valuta osv. 
-          // Hvis egenskaben findes på elementet, så vis det. Hvis ikke, så vis en tom tekst.
-          if (element[egenskab] !== undefined) {
+      egenskab.forEach(egenskab => {  //løber igennem alle elementer i arrayet 
+          if (element[egenskab] !== undefined) { //vis værdien hvis elementet har den givne egenskab
               html += `<div>${element[egenskab]}</div>`;
           } else {
-              html += `<div></div>`; // Hvis feltet mangler, laver vi bare en tom div
+              html += `<div></div>`; //indsætter en tom div hvis egenskaben mangler
           }
       });
 
-      html += `</div>`; // Slutter rækken
+      html += `</div>`; //Aflutter rækken
   });
 
-  return html; // Returnerer alt den genererede HTML
+  return html; // Returnerer den samlede HTML string 
 }
 
